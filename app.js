a5169971f24b76081d546c28ef8c7eaac3ef7c0f
Sassyhamza1 <hamza.iintellect@gmail.com>
2020-11-23 21:46:18 +0500

    asd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'))
 
 app.listen(3000,(req,res)=>{
 
