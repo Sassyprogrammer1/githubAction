879383dfcf0dd941596cd933f8cdb496d404f527
Sassyhamza1 <hamza.iintellect@gmail.com>
2020-11-23 15:16:55 +0500

    new one

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.use(express.static('public'))
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.listen(3000,(req,res)=>{
-    
+
     console.log("server is listening on port 3000")
 })
